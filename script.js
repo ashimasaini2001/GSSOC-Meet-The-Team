740399064cd92c45d150138a61dc99ec7643d5d7
Ashima <ashimasaini0101@outlook.com>
2024-10-04 23:00:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,225 @@
+
+    const teamMembers = [
+        {
+            name: "Anubha Maneshwar", 
+            title: "Founder & CEO,GirlScript Foundation", 
+            image: "./teamPics/Anubha_Maneshwar.png", 
+            github: "https://github.com/anubhamane",
+            twitter: "https://twitter.com/anubhamane",
+            linkedin: "https://www.linkedin.com/in/anubhamane/"
+        },
+        {
+            name: "Mohit Varu", 
+            title: "MD GirlScript Foundation", 
+            image: "./teamPics/Mohit_Varu.png", 
+            twitter: "https://twitter.com/13_Saanjh",
+            linkedin: "https://www.linkedin.com/in/mohitvaru/"
+        },
+        {
+            name: "Ojas Rajankar", 
+            title: "VP Engineering", 
+            image: "./teamPics/Ojas_Rajankar.png", 
+            github: "https://github.com/theweb3kid",
+            twitter: "https://x.com/ojasrajankar",
+            linkedin: "https://www.linkedin.com/in/ojasrajankar/"
+        },
+        {
+            name: "Sowmiya V", 
+            title: "Assistant Operations Manager", 
+            image: "./teamPics/Sowmiya_V.png", 
+            github: "https://github.com/sowmiyeh",
+            twitter: "https://x.com/sowmiyeh",
+            linkedin: "https://www.linkedin.com/in/sowmiyavenkatesan/"
+        },
+        {
+            name: "Sanjay Viswanathan", 
+            title: "Senior Program Manager", 
+            image: "./teamPics/Sanjay_Viswanathan.png", 
+            github: "https://github.com/sanjay-kv",
+            twitter: "https://twitter.com/sanjay_kv_",
+            linkedin: "https://www.linkedin.com/in/sanjay-k-v/"
+        },
+        {
+            name: "Mastan Sayyad", 
+            title: "Program Manager", 
+            image: "./teamPics/Mastan_Sayyad.png", 
+            github: "https://github.com/MastanSayyad",
+            linkedin: "https://www.linkedin.com/in/mastan-sayyad-126904223/"
+        },
+        {
+            name: "Radhika Malpani", 
+            title: "Program Manager", 
+            image: "./teamPics/Radhika_Malpani.png", 
+            github: "https://github.com/RadhikaMalpani1702",
+            linkedin: "https://www.linkedin.com/in/radhikamalpani1702/"
+        },
+        {
+            name: "Suhani Singh Paliwal", 
+            title: "Program Manager", 
+            image: "./teamPics/Suhani_Singh_Paliwal.png", 
+            github: "https://github.com/suhanipaliwal",
+            twitter: "https://x.com/suhanispaliwal",
+            linkedin: "https://www.linkedin.com/in/suhani-singh-paliwal/"
+        },
+        {
+            name: "Hemanth Kumar", 
+            title: "Program Manager", 
+            image: "./teamPics/Hemanth_Kumar.png", 
+            github: "https://github.com/Hemu21",
+            twitter: "https://x.com/Hemu21_",
+            linkedin: "https://www.linkedin.com/in/hemu21"
+        },
+        {
+            name: "Vansh Chaurasiya", 
+            title: "Intern", 
+            image: "./teamPics/Vansh_Chaurasiya.png", 
+            github: "https://github.com/vansh-codes",
+            twitter: "https://x.com/VanshChaurasiy4",
+            linkedin: "https://www.linkedin.com/in/vanshchaurasiya24/"
+        },
+        {
+            name: "Shreyash Srivastava", 
+            title: "Intern", 
+            image: "./teamPics/Shreyash_Srivastava.png", 
+            github: "https://github.com/shreyash3087",
+            twitter: "https://x.com/Shreyash3087",
+            linkedin: "https://www.linkedin.com/in/shreyash-srivastava-1a1161280"
+        },
+        {
+            name: "Meet Jain", 
+            title: "Web Developer", 
+            image: "./teamPics/Meet_Jain.png", 
+            github: "https://github.com/Meetjain1",
+            twitter: "https://twitter.com/Meetjain_100",
+            linkedin: "https://linkedin.com/in/meet-jain-413015265/"
+        },
+        {
+            name: "Aslam Sayyad", 
+            title: "Web Developer", 
+            image: "./teamPics/Aslam_Sayyad.png", 
+            github: "https://github.com/aslams2020",
+            twitter: "https://x.com/Aslams_02",
+            linkedin: "https://www.linkedin.com/in/aslamsayyad02/"
+        },
+        {
+            name: "Dinesh Talwadker", 
+            title: "Web Developer", 
+            image: "./teamPics/Dinesh_Talwadker.png", 
+            github: "https://github.com/dinxsh",
+            twitter: "https://twitter.com/dineshcodes",
+            linkedin: "https://linkedin.com/in/dineshtalwadker"
+        },
+        {
+            name: "Kalyani Uppuluri", 
+            title: "Leaderboard Specialist", 
+            image: "./teamPics/Kalyani_Uppuluri.png", 
+            github: "https://www.github.com/UppuluriKalyani",
+            twitter: "https://x.com/_Kalyaniangel_?s=093",
+            linkedin: "https://www.linkedin.com/in/kalyani-uppuluri-522419257/"
+        },
+        {
+            name: "Shreya Paul", 
+            title: "Public Relations Officer", 
+            image: "./teamPics/Shreya_Paul.png", 
+           
+            github: "https://github.com/shreya-paul-17",
+
+            linkedin: "https://www.linkedin.com/in/shreya-paul17/"
+        },
+        {
+            name: "Siddheya Kulkarni", 
+            title: "Public Relations Officer", 
+            image: "./teamPics/Siddheya_Kulkarni.png", 
+            github: "https://github.com/Asymtode712",
+            twitter: "https://x.com/SidKulkarni712",
+            linkedin: "https://www.linkedin.com/in/siddheya-kulkarni/"
+        },
+        {
+            name: "Aditya Bhaumik", 
+            title: "Community Manager", 
+            image: "/teamPics/Aditya_Bhaumik.png", 
+            github:" https://github.com/aditya-bhaumik",
+            twitter: "https://twitter.com/cricketaditya30",
+            linkedin: "https://www.linkedin.com/in/aditya-bhaumik-62b6b2220/"
+        },
+        {
+            name: "Chiluka Akshitha", 
+            title: "Community Manager", 
+            image: "/teamPics/Chiluka_Akshitha.png", 
+            github: "https://github.com/AKSHITHA-CHILUKA",
+            twitter: "https://x.com/AkshithaChiluka",
+            linkedin: "https://www.linkedin.com/in/akshitha-chiluka-b19245259"
+        },
+        {
+            name: "Sneha Kumari", 
+            title: "Community Manager", 
+            image: "/teamPics/Sneha_Kumari.png", 
+            github: "https://github.com/Snehsk",
+            twitter: "https://x.com/SnehaKumariSk1",
+            linkedin: "https://www.linkedin.com/in/sneha-kumari1/"
+        },
+        {
+            name: "Anshika Tiwari", 
+            title: "Content Writer", 
+            image: "/teamPics/Anshika_Tiwari.png", 
+            github: "https://github.com/anshikatiwari20",
+            twitter: "#https://x.com/anshikat_20",
+            linkedin: "https://www.linkedin.com/in/anshika-tiwari-/"
+        },
+        {
+            name: "Tejashri Tral", 
+            title: "Sponsorship Core", 
+            image: "/teamPics/Tejashri_Taral.png", 
+            github: "https://github.com/Tejashri-Taral",
+            twitter: "https://x.com/Tejashri_10",
+            linkedin: "https://www.linkedin.com/in/tejashri-taral-5006a82a0/"
+        },
+        {
+            name: "Saumili Roy", 
+            title: "UI/UX Designer", 
+            image: "/teamPics/Saumili_Roy.png", 
+            github: "https://github.com/sau-mili",
+            twitter: "https://twitter.com/SaumiliR",
+            linkedin: "https://www.linkedin.com/in/saumili-roy/"
+        },
+        {
+            name: "Garima Singh", 
+            title: "Discord Manager", 
+            image: "/teamPics/Garima_Singh.png", 
+            github: "https://github.com/GarimaSingh0109",
+            twitter: "https://x.com/GarimaS79949131",
+            linkedin: "https://www.linkedin.com/in/garima-singh-279014264"
+        },
+        {
+            name: "Hemant Panwar", 
+            title: "Discord Manager", 
+            image: "/teamPics/Hemant_Panwar.png", 
+            github: "https://github.com/Hemantlohar",
+            twitter: "https://twitter.com/_Hemant_lohar_",
+            linkedin: "https://www.linkedin.com/in/hemant-lohar-40195317a/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+        },
+    ]
+    const teamContainer = document.getElementById('team-container');
+    let teamCardsHTML = '';
+    
+    teamMembers.forEach(member => {
+        teamCardsHTML += `
+            <div class="col-md-4">
+                <div class="team-card">
+                    <img src="${member.image}" alt="${member.name}" class="team-img">
+                    <div class="card-body">
+                        <h5 class="card-title">${member.name}</h5>
+                        <p class="card-text">${member.title}</p>
+                        <ul class="icon-list">
+                            ${member.github ? `<li><a href="${member.github}" aria-label="GitHub"><i class="fab fa-github icon"></i></a></li>` : ''}
+                            ${member.twitter ? `<li><a href="${member.twitter}" aria-label="Twitter"><i class="fab fa-x-twitter icon"></i></a></li>` : ''}
+                            ${member.linkedin ? `<li><a href="${member.linkedin}" aria-label="LinkedIn"><i class="fab fa-linkedin icon"></i></a></li>` : ''}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+    
+
+    teamContainer.innerHTML = teamCardsHTML;
